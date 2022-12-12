31422cf0f4cd3709bbc8c2c6caf74cb8c4a531bc
WillCui- <wyfcui@gmail.com>
2022-12-12 17:23:21 -0500

    extract-segments test

--- egs/fsr/s5/run.sh
+++ egs/fsr/s5/run.sh
@@ -1,10 +1,14 @@
 . ./utils/parse_options.sh
 . ./cmd.sh
 . ./path.sh
+. ./utils/parse_options.sh
 
 nj=1
 cmd=$train_cmd
 logdir=./log
 
-$cmd JOB=1:$nj $logdir/test.JOB.log \
-  mel-computations 
+name=`basename`
+scp=data/train/wav.scp
+
+$cmd JOB=1:$nj $logdir/make_mfcc_${name}.JOB.log \
+    extract-segments scp,p:$scp $logdir/segments.JOB ark:-
